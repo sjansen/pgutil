34f35df22a9a347be63ac45211c519babc25b8ea
Stuart Jansen <sjansen@buscaluz.org>
2019-02-05 18:43:48 -0700

    add basic connect options

--- internal/commands/ping.go
+++ internal/commands/ping.go
@@ -3,36 +3,50 @@ package commands
 import (
 	"fmt"
 
-	"github.com/sjansen/pgutil/internal/db"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/sjansen/pgutil/internal/db"
 )
 
-/* TODO connection args
--h host
---host=host
--p port
---port=port
--d dbname
---dbname=dbname
--U username
---username=username
--P username
---password=password
-*/
 type pingCmd struct {
+	host     string
+	port     string
+	dbname   string
+	username string
+	// TODO password string
 }
 
-func (v *pingCmd) register(app *kingpin.Application) {
-	app.Command("ping", "Create a test connection to a database").Action(v.run)
+func (c *pingCmd) register(app *kingpin.Application) {
+	cmd := app.Command("ping", "Create a test connection to a database").Action(c.run)
+	cmd.Flag("host", "database server host or socket directory").Short('h').StringVar(&c.host)
+	cmd.Flag("port", "database server port").Short('p').StringVar(&c.port)
+	cmd.Flag("dbname", "database server port").Short('d').StringVar(&c.dbname)
+	cmd.Flag("username", "connect as username").Short('U').StringVar(&c.username)
 }
 
-func (v *pingCmd) run(pc *kingpin.ParseContext) error {
-	c, err := db.New(nil)
+func (c *pingCmd) run(pc *kingpin.ParseContext) error {
+	options := map[string]string{}
+
+	if c.host != "" {
+		if c.port != "" {
+			options["addr"] = c.host + ":" + c.port
+		} else {
+			options["addr"] = c.host + ":5432"
+		}
+	}
+	if c.dbname != "" {
+		options["database"] = c.dbname
+	}
+	if c.username != "" {
+		options["user"] = c.username
+	}
+
+	db, err := db.New(options)
 	if err != nil {
 		return err
 	}
 
-	version, err := c.ServerVersion()
+	version, err := db.ServerVersion()
 	if err == nil {
 		fmt.Println(version)
 	}
