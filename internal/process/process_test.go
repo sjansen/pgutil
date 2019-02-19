d29fd8c8b89b7572a8807bb95af55cd37fba857d
Stuart Jansen <sjansen@buscaluz.org>
2019-02-18 18:12:50 -0700

    improve internal/process/ test coverage

--- internal/process/process_test.go
+++ internal/process/process_test.go
@@ -15,16 +15,29 @@ func TestRun(t *testing.T) {
 	expected := "Kilroy was here.\n"
 	var stdout, stderr bytes.Buffer
 	c := process.Command{
-		Args: []string{
-			"echo", "Kilroy", "was", "here.",
-		},
+		Args: []string{"echo", "Kilroy", "was", "here."},
 
 		Stdout: &stdout,
 		Stderr: &stderr,
 	}
-
 	err := c.Run()
 	require.NoError(err)
 	require.Equal(expected, stdout.String())
+	require.Empty(stderr.String())
+
+	c.Args = []string{"non-existent-command"}
+	stdout.Reset()
+	stderr.Reset()
+	err = c.Run()
+	require.Error(err)
+	require.Empty(stdout.String())
+	require.Empty(stderr.String())
+
+	c.Args = []string{"scripts/non-existent-command"}
+	stdout.Reset()
+	stderr.Reset()
+	err = c.Run()
+	require.Error(err)
+	require.Empty(stdout.String())
 	require.Empty(stderr.String())
 }
