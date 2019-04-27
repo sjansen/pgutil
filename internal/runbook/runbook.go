3023378bb421b72dd2d354681ed4e1c3479cfacb
Stuart Jansen <sjansen@buscaluz.org>
2019-04-27 14:29:19 -0600

    wrap runbook scheduler in a goroutine

--- internal/runbook/runbook.go
+++ internal/runbook/runbook.go
@@ -1,1 +1,44 @@
 package runbook
+
+import (
+	"github.com/sjansen/pgutil/internal/runbook/scheduler"
+	"github.com/sjansen/pgutil/internal/runbook/types"
+)
+
+func newCompletedChan(targets types.Targets) chan types.TaskID {
+	capacity := 0
+	for _, t := range targets {
+		capacity += t.ConcurrencyLimit()
+	}
+	capacity *= 2
+	return make(chan types.TaskID, capacity)
+}
+
+func startScheduler(targets types.Targets, tasks types.Tasks, completed <-chan types.TaskID) <-chan types.TaskID {
+	ch := make(chan types.TaskID)
+
+	go func(ch chan<- types.TaskID) {
+		s, ready, err := scheduler.New(targets, tasks)
+		for {
+			if err != nil {
+				break
+			}
+			for _, taskIDs := range ready {
+				for _, taskID := range taskIDs {
+					ch <- types.TaskID(taskID)
+				}
+			}
+			if taskID, ok := <-completed; ok {
+				ready, err = s.Next(string(taskID))
+			} else {
+				break
+			}
+		}
+		close(ch)
+		for range completed {
+			// drain completed
+		}
+	}(ch)
+
+	return ch
+}
---
+++ internal/runbook/runbook_internal_test.go
@@ -0,0 +1,89 @@
+package runbook
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/sjansen/pgutil/internal/runbook/types"
+)
+
+var targets = types.Targets{
+	"t1": &fakeTarget{},
+	"t2": &fakeTarget{},
+}
+var tasks = types.Tasks{
+	"a": {Target: "t1"},
+	"b": {Target: "t2"},
+	"c": {Target: "t1", After: []string{"b"}},
+	"d": {Target: "t2", After: []string{"a", "c"}},
+	"e": {Target: "t1", After: []string{"d"}},
+	"f": {Target: "t1", After: []string{"a", "b", "e"}},
+	"g": {Target: "t1", After: []string{"b", "e"}},
+	"h": {Target: "t2", After: []string{"g"}},
+	"i": {Target: "t2", After: []string{"g"}},
+	"j": {Target: "t2", After: []string{"g"}},
+}
+
+func TestScheduler(t *testing.T) {
+	require := require.New(t)
+
+	completed := newCompletedChan(targets)
+	ready := startScheduler(targets, tasks, completed)
+
+	seen := map[types.TaskID]struct{}{}
+	for taskID := range ready {
+		seen[taskID] = struct{}{}
+		completed <- taskID
+	}
+
+	expected := map[types.TaskID]struct{}{
+		"a": {}, "b": {}, "c": {}, "d": {}, "e": {},
+		"f": {}, "g": {}, "h": {}, "i": {}, "j": {},
+	}
+	require.Equal(expected, seen)
+}
+
+func TestSchedulerEarlyTermination(t *testing.T) {
+	require := require.New(t)
+	completed := newCompletedChan(targets)
+	ready := startScheduler(targets, tasks, completed)
+
+	seen := map[types.TaskID]struct{}{}
+	for taskID := range ready {
+		seen[taskID] = struct{}{}
+		if taskID == "e" {
+			close(completed)
+			break
+		} else {
+			completed <- taskID
+		}
+	}
+
+	expected := map[types.TaskID]struct{}{
+		"a": {}, "b": {}, "c": {}, "d": {}, "e": {},
+	}
+	require.Equal(expected, seen)
+}
+
+type fakeTarget struct{}
+
+func (t *fakeTarget) Analyze() error {
+	return nil
+}
+func (t *fakeTarget) ConcurrencyLimit() int {
+	return 1
+}
+func (t *fakeTarget) Handle(ctx context.Context, task types.TaskConfig) error {
+	return nil
+}
+func (t *fakeTarget) NewTaskConfig(class string) (types.TaskConfig, error) {
+	return nil, nil
+}
+func (t *fakeTarget) Start() error {
+	return nil
+}
+func (t *fakeTarget) Stop() error {
+	return nil
+}
