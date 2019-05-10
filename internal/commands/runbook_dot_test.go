b51f02ff44a1fc1aa7094b72cb4e681824c21294
Stuart Jansen <sjansen@buscaluz.org>
2019-05-10 07:07:47 -0600

    add test coverage for runbook dot command

---
+++ internal/commands/runbook_dot_test.go
@@ -0,0 +1,33 @@
+package commands_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/sjansen/pgutil/internal/commands"
+	"github.com/sjansen/pgutil/internal/logger"
+	"github.com/sjansen/pgutil/internal/sys"
+)
+
+func TestRunBookDot(t *testing.T) {
+	require := require.New(t)
+
+	cmd := &commands.RunBookDotCmd{
+		File:   "testdata/message.jsonnet",
+		Output: "-",
+	}
+	var stdout, stderr bytes.Buffer
+	base := &commands.Base{
+		IO: sys.IO{
+			Log:    logger.Discard(),
+			Stdout: &stdout,
+			Stderr: &stderr,
+		},
+	}
+	err := cmd.Run(base)
+	require.NoError(err)
+	require.NotEmpty(stdout.String())
+	require.Empty(stderr.String())
+}
