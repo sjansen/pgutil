8dd420d1cf5c7e7f9feb0092d63613cddd2de3e7
Stuart Jansen <sjansen@buscaluz.org>
2019-05-15 19:10:25 -0600

    improve trigger parser

--- internal/ddl/trigger_test.go
+++ internal/ddl/trigger_test.go
@@ -4,6 +4,7 @@
 package ddl
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/alecthomas/participle"
@@ -14,8 +15,11 @@ import (
 var (
 	triggerLexer = lexer.Must(lexer.Regexp(`(\s+)` +
 		`|(?P<Keyword>(?i)` +
-		`AFTER|BEFORE|CONSTRAINT|CREATE|DELETE|EACH|EXECUTE` +
-		`|FOR|INSERT|INSTEAD|OF|ON|OR|PROCEDURE|ROW|STATEMENT` +
+		`AFTER|BEFORE|CONSTRAINT|CREATE` +
+		`|DEFERRABLE|DEFERRED|DELETE` +
+		`|EACH|EXECUTE\s+PROCEDURE|FOR|FROM` +
+		`|IMMEDIATE|INITIALLY|INSERT|INSTEAD\s+OF` +
+		`|NOT|ON|OR|ROW|STATEMENT` +
 		`|TRIGGER|TRUNCATE|UPDATE` +
 		`)\b` +
 		`|(?P<Ident>[a-zA-Z_][a-zA-Z0-9_]*)` +
@@ -27,19 +31,30 @@ var (
 		&createTrigger{},
 		participle.Lexer(triggerLexer),
 		participle.CaseInsensitive("Keyword"),
+		participle.Map(CollapseWhitespace, "Keyword"),
 		participle.Unquote("String"),
 		participle.Upper("Keyword"),
 	)
+
+	whitespace = regexp.MustCompile(`\s+`)
 )
+
+func CollapseWhitespace(token lexer.Token) (lexer.Token, error) {
+	token.Value = whitespace.ReplaceAllString(token.Value, " ")
+	return token, nil
+}
 
 type createTrigger struct {
 	Constraint bool            `"CREATE" @"CONSTRAINT"? "TRIGGER"`
 	Name       string          `@Ident`
-	When       string          `@("BEFORE"|"AFTER"|"INSTEAD" "OF")`
+	When       string          `@("BEFORE"|"AFTER"|"INSTEAD OF")`
 	Events     []*triggerEvent `@@ ("OR" @@)*`
 	Table      string          `"ON" @Ident`
+	From       string          `("FROM" @Ident)?`
+	Deferrable bool            `("NOT" "DEFERRABLE"|@"DEFERRABLE"`
+	Deferred   bool            ` ("INITIALLY" "IMMEDIATE"|"INITIALLY" @"DEFERRED")?)?`
 	ForEach    string          `("FOR" "EACH"? @("ROW"|"STATEMENT"))?`
-	Function   string          `"EXECUTE" "PROCEDURE" @Ident "(" ")"`
+	Function   string          `"EXECUTE PROCEDURE" @Ident "(" ")"`
 }
 
 type triggerEvent struct {
@@ -50,26 +65,89 @@ type triggerEvent struct {
 func TestTriggerParser(t *testing.T) {
 	require := require.New(t)
 
-	actual := &createTrigger{}
-	err := triggerParser.ParseString(
-		`CREATE TRIGGER update_foo_modified
+	for _, tc := range []struct {
+		Stmt     string
+		Expected *createTrigger
+	}{{Stmt: `
+		  CREATE TRIGGER update_foo_modified
 		  BEFORE UPDATE ON foo
 		  FOR EACH ROW
 		  EXECUTE PROCEDURE update_modified_column()
 		`,
-		actual,
-	)
-	require.NoError(err)
-
-	expected := &createTrigger{
-		Name: "update_foo_modified",
-		When: "BEFORE",
-		Events: []*triggerEvent{
-			{Event: "UPDATE"},
+		Expected: &createTrigger{
+			Name: "update_foo_modified",
+			When: "BEFORE",
+			Events: []*triggerEvent{
+				{Event: "UPDATE"},
+			},
+			Table:    "foo",
+			ForEach:  "ROW",
+			Function: "update_modified_column",
 		},
-		Table:    "foo",
-		ForEach:  "ROW",
-		Function: "update_modified_column",
+	}, {Stmt: `
+		  CREATE TRIGGER trigger1
+		  AFTER DELETE ON table1
+		  EXECUTE PROCEDURE fn1()
+		`,
+		Expected: &createTrigger{
+			Name: "trigger1",
+			When: "AFTER",
+			Events: []*triggerEvent{
+				{Event: "DELETE"},
+			},
+			Table:    "table1",
+			Function: "fn1",
+		},
+	}, {Stmt: `
+		  CREATE CONSTRAINT TRIGGER trigger2
+		  INSTEAD OF INSERT OR TRUNCATE ON table2
+		  DEFERRABLE INITIALLY DEFERRED
+		  FOR EACH STATEMENT
+		  EXECUTE PROCEDURE fn2()
+		`,
+		Expected: &createTrigger{
+			Constraint: true,
+			Name:       "trigger2",
+			When:       "INSTEAD OF",
+			Events: []*triggerEvent{
+				{Event: "INSERT"},
+				{Event: "TRUNCATE"},
+			},
+			Table:      "table2",
+			Deferrable: true,
+			Deferred:   true,
+			ForEach:    "STATEMENT",
+			Function:   "fn2",
+		},
+	}, {Stmt: `
+		  create  constraint  trigger  TRIGGER3
+		  instead  of  insert  or  truncate  on  TABLE3
+		  deferrable  initially  deferred
+		  for  each  statement
+		  execute  procedure  FN3()
+		`,
+		Expected: &createTrigger{
+			Constraint: true,
+			Name:       "TRIGGER3",
+			When:       "INSTEAD OF",
+			Events: []*triggerEvent{
+				{Event: "INSERT"},
+				{Event: "TRUNCATE"},
+			},
+			Table:      "TABLE3",
+			Deferrable: true,
+			Deferred:   true,
+			ForEach:    "STATEMENT",
+			Function:   "FN3",
+		},
+	},
+	} {
+		actual := &createTrigger{}
+		err := triggerParser.ParseString(
+			tc.Stmt,
+			actual,
+		)
+		require.NoError(err, tc.Stmt)
+		require.Equal(tc.Expected, actual, tc.Stmt)
 	}
-	require.Equal(expected, actual)
 }
